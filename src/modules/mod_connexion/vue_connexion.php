2ea1afad5d290e037e7e448f2b07f39947472715
Rayan <rchouchane@iut.univ-paris8.fr>
2022-10-22 14:40:16 +0200

    simple changement du style css

--- src/modules/mod_connexion/vue_connexion.php
+++ src/modules/mod_connexion/vue_connexion.php
@@ -50,6 +50,8 @@ class VueConnexion
     </div>
     </div>
 </header>
+
+
 <?php
   }
 
@@ -72,7 +74,8 @@ class VueConnexion
       <div> <input type="text" placeholder="Identifiant" name="identifiant" required></div>
       <div > <input type="text" placeholder="Mot de passe" name="motDePasse"required></div>
       <div><input type="email" placeholder="E-mail" name="adresseMail"required></div>
-      <div><input type="submit" value="Inscription!"> </div>
+      <div><input type="submit" value="S'inscrire 👋🏻 !"> </div>
+      <p>© 2022–2023</p>
     </form>
     <div>
       <div class="drop drop-1"></div>
@@ -94,31 +97,31 @@ class VueConnexion
     ?>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset='utf-8'>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  </head>
-  <body>
-    
-  <div class="container">
-      <div class="alert alert-warning alert-dismissible fade show" role="alert">
-      <h4>Inscription Réussite !!! 😍</h4>
-      Bonjour 
-      <?php echo$_POST['identifiant']; ?>  
-      et bienvenue sur A2Z la plateforme intuitive pour créer sa fiche d'exercice 😄!
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">x</span></button>
-      </div>
-    </div>  
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset='utf-8'>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      </head>
+      <body>
+        
+      <div class="container">
+          <div class="alert alert-warning alert-dismissible fade show" role="alert">
+          <h4>Inscription Réussite !!! 😍</h4>
+          Bonjour 
+          <?php echo$_POST['identifiant']; ?>  
+          et bienvenue sur A2Z la plateforme intuitive pour créer sa fiche d'exercice 😄!
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">x</span></button>
+          </div>
+        </div>  
 
-  </body>
-</html>
+      </body>
+    </html>
 
 <?php
 }
@@ -138,6 +141,7 @@ class VueConnexion
         <div><input type="text" placeholder="Mot de passe" name="motDePasse"required></div>
         <div><input type="submit" value="Connexion!"></div>
           <a href="#">Mot de passe oublié</a>
+          <p>© 2022–2023</p>
        </form>
         <div class="drop drop-1"></div>
         <div class="drop drop-2"></div>
