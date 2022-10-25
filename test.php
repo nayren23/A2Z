2e629bf9c833919cd02a14a40932c437c7d50908
Aldric Claude <aldric.cld@gmail.com>
2022-10-25 15:10:42 +0200

    Navbar + début de carousel

---
+++ test.php
@@ -0,0 +1,45 @@
+<!DOCTYPE htlm>
+<html> 
+   <head>
+        <!-- CSS only -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="t.css">
+        <style>
+    
+    </style>
+    </head>
+
+    <body>
+
+    <nav class="p-3 mb-3 border-bottom">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <img class="logo" src="TabA2Z.png" width="64" height="64">
+          <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="#" class="nav-link px-2 link-secondary">Accueil</a></li>
+            <li><a href="#" class="nav-link px-2 link-dark">Mes Fiches</a></li>
+            <li><a href="#" class="nav-link px-2 link-dark">Fiches publiques</a></li>
+          </ul>
+        
+        
+      </div>
+          <div class="photoProfil" >
+            <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="/~aclaude/Sae/pdp.jpeg" alt="mdo" width="48" height="48" class="rounded-circle">
+            </a>
+            <ul class="dropdown-menu text-small">
+              <li><a class="dropdown-item" href="#">Profile</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Déconnexion</a></li>
+            </ul>
+          </div>
+      </div>
+    </div>
+</nav>
+
+
+      <!-- JavaScript Bundle with Popper -->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+    </body>
+</html>
