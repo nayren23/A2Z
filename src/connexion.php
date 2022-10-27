5befbfa287219434014ab71349d841d32339c949
Rayan <rchouchane@iut.univ-paris8.fr>
2022-10-27 19:19:03 +0200

    ajout d'une ligne dans connexion pour utiliser la bdd depuis chez moi

--- src/connexion.php
+++ src/connexion.php
@@ -9,7 +9,8 @@ class Connexion{
 
     public static function initConnexion(){
 
-        self :: $bdd = new PDO('mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201619', 'dutinfopw201619', 'gysezypu'); // on met self :: car c'est champ statique à l’intérieur d’une classe
+        //self :: $bdd = new PDO('mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201619', 'dutinfopw201619', 'gysezypu'); // on met self :: car c'est champ statique à l’intérieur d’une classe
+        self :: $bdd = new PDO('mysql:host=localhost;dbname=dutinfopw201620', 'dutinfopw201620', 'dejyjamu');  // pour rayan NE PAS SUPPRIMER ET UTILISER L AUTRE BDD
 
     }
 }
