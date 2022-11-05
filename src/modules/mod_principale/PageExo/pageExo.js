27fc7e8d90bcc09adb2c8da065e3d752eb295d5e
Yassine <yhamidi@iut.univ-paris8.fr>
2022-11-05 12:03:13 +0100

    Ajout d'une liste deroulante pour choisir sont font et amélioration des choses qui pop sur la page une fois un exercice drag

--- src/modules/mod_principale/PageExo/pageExo.js
+++ src/modules/mod_principale/PageExo/pageExo.js
@@ -23,8 +23,67 @@ $(function () {
 $(function () {
     $(".dropp2").droppable();
     $(".dragg").draggable({
+        connectToSortable: ".sortable",
+        helper: "clone",
+        revert: "invalid",
+        
         drag: function (event, ui) {
-            $(".res").html("<b>Draggable Drag.</b><br>");
+            $(".res").html("<b>Draggable Drag.</b><br><p> salut je suis moi </p>");
         }
+        
     });
+    
+
 });
+
+
+
+
+function changeFont() {
+    var elements = document.getElementsByClassName('font');
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        element.style.fontFamily = "cursive";
+
+    }
+}
+
+
+
+
+
+function changeBold() {
+    var elements = document.getElementsByClassName('bold');
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        element.style.fontWeight = "bolder";
+
+    }
+}
+
+
+
+
+
+function changeNormal() {
+    var elements = document.getElementsByClassName('normal');
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        element.style.fontWeight = "normal";
+
+    }
+}
+
+function changeAll(font) {
+    var elements = document.getElementsByClassName('all');
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        element.style.fontFamily = font.value;
+
+    }
+}
+
