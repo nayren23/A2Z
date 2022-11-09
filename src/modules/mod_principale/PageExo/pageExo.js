042e7fa63fc45d83865d1fa48b9bc1590c3b01c7
Yassine <chefyaya93@outlook.com>
2022-11-09 15:06:48 +0100

    ajout d'un test pour faire apparaitre des exos une fois que le drag sur la page est finit

--- src/modules/mod_principale/PageExo/pageExo.js
+++ src/modules/mod_principale/PageExo/pageExo.js
@@ -1,5 +1,15 @@
 
 
+/*changement de la police*/
+function changeAll(font) {
+    var elements = document.getElementsByClassName('all');
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        element.style.fontFamily = font.value;
+
+    }
+}
 
 
 $(function () {
@@ -41,35 +51,22 @@ $(function () {
 
 
 
-function changeAll(font) {
-    var elements = document.getElementsByClassName('all');
-
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        element.style.fontFamily = font.value;
-
-    }
-}
 
 
-
-/*
-
-
-$(function() {
-    $( ".draggable, #draggable-nonvalid" ).draggable();
+$( function() {
+    $( "#draggable, #draggable-nonvalid" ).draggable();
     $( "#droppable" ).droppable({
-      accept: ".draggable",
+      accept: "#draggable",
       classes: {
         "ui-droppable-active": "ui-state-active",
         "ui-droppable-hover": "ui-state-hover"
       },
       drop: function( event, ui ) {
-        $(".res").append('<span class="accordion-heading all" id="myElement">Mise en page</span>');
+        $( this )
+          .addClass( "ui-state-highlight" )
+
+            $(".res").append('<span class="accordion-heading all" id="myElement">Mise en page</span>');
 
       }
     });
-  });
-*/ 
-
-
+  } );
