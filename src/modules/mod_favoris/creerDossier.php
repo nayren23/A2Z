1cdb22eaca893712558d617a823f0f62b601420e
Aldric Claude <aldric.cld@gmail.com>
2022-11-16 12:47:43 +0100

    Patch Dossier

--- src/modules/mod_favoris/creerDossier.php
+++ src/modules/mod_favoris/creerDossier.php
@@ -3,8 +3,12 @@ header('Content-Type: application/json; charset=utf-8');
 echo json_encode(isset($_POST["dossier"])? $_POST["dossier"] : "null");
 
 $sql = 'INSERT INTO Dossier(nomDossier, idParent, idUser) VALUES ( ? , ?, ?)';
-$preparedStatment = 
-$statement = self::$bdd->prepare($sql);
+$stmt = self::$bdd->prepare($sql);
+$nomDossier = $_POST["dossier"];
+$idParent = 
+$idUser = 
+
+header('Location: ./index.php?module=favoris&dossierCourant='.$nomDossier.';'); //redirection vers la page 
 
 
 ?>
