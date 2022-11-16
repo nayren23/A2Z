1c7484a839eb78a54ef3c17d378957aaf9bb45d7
Aldric Claude <aldric.cld@gmail.com>
2022-11-16 12:19:48 +0100

    Tesst adresse email principale

--- src/Script_js/script_dossier.js
+++ src/Script_js/script_dossier.js
@@ -17,13 +17,11 @@ async function popUpNomDuDossier() {
            data : { dossier : nomDossier  } ,
            dataType : "json"
            })
-           
+        
            .done ( function ( retour ) {
             console.log("ok")
            alert( " Reponse : " +retour ) ;
           10 } ) ;
-
-          //header('Location: ./index.php?module=favoris&dossier=;'); //redirection vers la page 
 
           
         })()
