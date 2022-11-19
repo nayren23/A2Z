bd57aa8538058f3b082416784ff51a0760ee6de5
Yassine <yhamidi@iut.univ-paris8.fr>
2022-11-19 17:08:42 +0100

    patch de la vue de la page Exo et reglage de quelque bugs

--- src/connexion.php
+++ src/connexion.php
@@ -9,12 +9,12 @@ class Connexion{
 
     public static function initConnexion(){
 
-        self :: $bdd = new PDO('mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201619', 'dutinfopw201619', 'gysezypu'); // on met self :: car c'est champ statique à l’intérieur d’une classe
+        //self :: $bdd = new PDO('mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201619', 'dutinfopw201619', 'gysezypu'); // on met self :: car c'est champ statique à l’intérieur d’une classe
         //pour rayan NE PAS SUPPRIMER ET UTILISER L AUTRE BDD
         //self :: $bdd = new PDO('mysql:host=localhost;dbname=dutinfopw201620', 'dutinfopw201620', 'dejyjamu');  
                 
         //pour yassine le BG NE PAS SUPPRIMER ET UTILISER L AUTRE BDD
-        //self::$bdd = new PDO('mysql:host=localhost;dbname=dutinfopw201620', 'yassine', 'yassine');
+        self::$bdd = new PDO('mysql:host=localhost;dbname=dutinfopw201620', 'yassine', 'yassine');
         
 
     }
